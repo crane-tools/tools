08928879eac8d26e89ffd8fba97d2f54ce1fdbb3
xiaohe.liu <xiaohe.liu@fcgyl.cn>
2016-08-08 18:15:37 +0800

    邮件发送帮助类

---
+++ src/main/java/crane/tools/EmailHelper.java
@@ -0,0 +1,45 @@
+package crane.tools;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by crane on 16/8/8.
+ */
+@Component
+public class EmailHelper {
+
+    @Value("${email.from}")
+    private String from;
+    @Value("${email.pwd}")
+    private String password;
+    @Value("${email.host}")
+    private String host;
+
+    public void send(String to, String subject, String content) throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "false");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "25");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject(subject);
+        message.setContent(content, "text/html; charset=utf-8");
+        Transport.send(message);
+    }
+}
--- src/test/java/crane/ApplicationTests.java
+++ src/test/java/crane/ApplicationTests.java
@@ -1,14 +1,21 @@
 package crane;
 
+import crane.tools.EmailHelper;
 import crane.tools.I18nManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.mail.MessagingException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
+
+    @Autowired
+    EmailHelper helper;
 
     @Test
     public void contextLoads() {
@@ -20,6 +27,11 @@ public class ApplicationTests {
         System.out.println(I18nManager.getInstance().getLang("key1"));
         I18nManager.getInstance().setLocaleByTag("en-US");
         System.out.println(I18nManager.getInstance().getFLang("key2", "param1", "param2"));
+    }
+
+    @Test
+    public void testMail() throws MessagingException {
+        helper.send("xxx@xxx.xxx","sdfas","dfasdf");
     }
 
 }
